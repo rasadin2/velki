9c69b1f5ff0b38e18297d55def0428cee864c0eb
Mrittunjoy <mrittunjoy@bitmascot.com>
2026-01-12 22:22:43 +0600

    Inner design

--- wp-content/themes/wicket/template-parts/menu/content-inline-menu.php
+++ wp-content/themes/wicket/template-parts/menu/content-inline-menu.php
@@ -35,16 +35,18 @@
 					?>
 					<!--<div class="right-button"><a href="<?php echo esc_url( home_url( '/জরুরি-যোগাযোগ/' ) ); ?>">জরুরি যোগাযোগ</a></div>-->
 					 <!--<div class="search-box"><?php echo do_shortcode( '[wpdreams_ajaxsearchlite]'); ?></div>-->
-				    <div class="search-box"><form role="search" method="get" class="search-form" action="https://www.leoberkeley.com/">
-				    <div class="search-box-wrap">
-					<input type="submit" class="search-submit" value="Search" />
-				     <label>
-					<span class="screen-reader-text">Search for:</span>
-					<input type="search" class="search-field" placeholder="এজেন্ট খুঁজুন..." value="" name="s" />
-				   </label>
+				    <div class="search-box">
+						<form role="search" method="get" class="search-form" action="http://localhost/velki/">
+							<div class="search-box-wrap">
+							<input type="submit" class="search-submit" value="Search" />
+							 <label>
+							<span class="screen-reader-text">Search for:</span>
+							<input type="search" class="search-field" placeholder="এজেন্ট খুঁজুন..." value="" name="s" />
+						   </label>
+						   </div>
+						</form>
+				  
 				   </div>
-			      </form>
-				  </div>
 				</div>
 				
 		    </div>
